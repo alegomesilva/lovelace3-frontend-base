2bef800911fdddf63b6cc176c8e14e6f08af4f5c
alegomesilva <89050647+alegomesilva@users.noreply.github.com>
2022-03-11 18:59:49 -0300

    finish

--- src/aboutme/script.js
+++ src/aboutme/script.js
@@ -4,3 +4,19 @@ document.querySelector(".card-btn").addEventListener('click', () => {
     console.log("Botão funciona")
     document.querySelector(".container").classList.toggle("change");
 })
+
+//  GET request using fetch()
+
+fetch("https://simple-market-backend.herokuapp.com/aboutme") //"https://jsonplaceholder.typicode.com/users"
+    .then(response => response.json())
+
+    .then(data => {
+        $('#reminder').append(`"${data.reminder}"`);
+        $('#greeting').append(`"${data.greeting}"`);
+        $('#aboutMe').append(`"${data.aboutMe}"`); 
+        $("#linkedin").append(`${data.linkedin}`);
+        $("#email").append(`${data.email}`);
+        $("#mobileNumber").append(`${data.mobileNumber}`);
+    });
+
+  
